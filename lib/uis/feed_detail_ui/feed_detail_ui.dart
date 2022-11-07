a23fc78595bb0bce9f451319462b5bb2e564f0e6
Bhuwan Shrestha <70331023+bxs8072@users.noreply.github.com>
2022-11-06 21:50:42 -0600

    feed detail page

--- lib/uis/feed_detail_ui/feed_detail_ui.dart
+++ lib/uis/feed_detail_ui/feed_detail_ui.dart
@@ -1,12 +1,20 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:tunes_lovers/models/feed.dart';
+import 'package:tunes_lovers/models/performer.dart';
 import 'package:tunes_lovers/models/person.dart';
+import 'package:tunes_lovers/pages/feeds_page/feeds_tags_ui.dart';
+import 'package:tunes_lovers/services/navigation_service/navigation_service.dart';
 import 'package:tunes_lovers/services/size_service/size_service.dart';
+import 'package:tunes_lovers/services/theme_service/dark_theme.dart';
+import 'package:tunes_lovers/services/theme_service/light_theme.dart';
 import 'package:tunes_lovers/services/theme_service/theme_service.dart';
 import 'package:tunes_lovers/uis/feed_detail_ui/comment_ui.dart';
+import 'package:tunes_lovers/uis/performer_detail_ui/performer_detail_ui.dart';
 
 class FeedDetailUI extends StatelessWidget {
   final Person person;
@@ -45,18 +53,257 @@ class FeedDetailUI extends StatelessWidget {
                             image: NetworkImage(feed.coverImage),
                           ),
                         ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            Intl()
+                                .date("MM.dd.yyyy HH:mm a")
+                                .format(feed.createdAt.toDate()),
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            feed.address.formattedAddress,
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Wrap(
+                          runSpacing: 10.0,
+                          spacing: 10.0,
+                          children: feed.genres
+                              .map(
+                                (e) => GestureDetector(
+                                  onTap: () {
+                                    NavigationService(context).push(
+                                      FeedsTagsUI(person: person, tag: e),
+                                    );
+                                  },
+                                  child: Chip(
+                                    backgroundColor:
+                                        Theme.of(context).cardTheme.color,
+                                    label: Text(
+                                      "#$e",
+                                      style: GoogleFonts.lato(
+                                        color: ThemeService.primaryColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                        const Divider(),
                         Text(
-                          feed.title,
-                          textAlign: TextAlign.center,
+                          "Performers",
+                          textAlign: TextAlign.start,
                           style: GoogleFonts.lato(
-                            fontSize: 20.0,
+                            fontSize: 14.0,
                             color: ThemeService.secondaryColor,
                             fontWeight: FontWeight.w600,
                           ),
                         ),
                         const SizedBox(
-                          height: SizeService.separatorHeight * 2,
+                          height: SizeService.separatorHeight,
                         ),
+                        SizedBox(
+                          height: SizeService(context).height * 0.28,
+                          child: ListView(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            children: feed.performers
+                                .map((String performerId) => StreamBuilder<
+                                        DocumentSnapshot>(
+                                    stream: FirebaseFirestore.instance
+                                        .collection("Performer")
+                                        .doc(performerId)
+                                        .snapshots(),
+                                    builder: (context, snapshot) {
+                                      if (!snapshot.hasData) {
+                                        return Center(
+                                          key: key,
+                                        );
+                                      }
+
+                                      Performer performer = Performer.fromJson(
+                                          snapshot.data!.data());
+                                      return GestureDetector(
+                                        onTap: () {
+                                          NavigationService(context).push(
+                                            PerformerDetailUI(
+                                                person: person,
+                                                performerId: performer.id),
+                                          );
+                                        },
+                                        child: AspectRatio(
+                                          aspectRatio: 14 / 8,
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                              vertical: SizeService
+                                                  .innerVerticalPadding,
+                                            ),
+                                            margin: const EdgeInsets.only(
+                                                right: 8.0),
+                                            decoration: BoxDecoration(
+                                              color: Theme.of(context)
+                                                  .cardTheme
+                                                  .color,
+                                              borderRadius:
+                                                  BorderRadius.circular(
+                                                      SizeService.borderRadius),
+                                              boxShadow: [
+                                                ThemeService.boxShadow(context),
+                                              ],
+                                            ),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Expanded(
+                                                  child: performer.avatarURL ==
+                                                          ""
+                                                      ? CircleAvatar(
+                                                          radius: 50.0,
+                                                          backgroundColor:
+                                                              Theme.of(context)
+                                                                  .primaryColor,
+                                                        )
+                                                      : Padding(
+                                                          padding:
+                                                              const EdgeInsets
+                                                                  .all(8.0),
+                                                          child: ClipRRect(
+                                                            borderRadius:
+                                                                BorderRadius.circular(
+                                                                    SizeService
+                                                                        .borderRadius),
+                                                            child:
+                                                                Image.network(
+                                                              performer
+                                                                  .avatarURL,
+                                                              fit: BoxFit.cover,
+                                                            ),
+                                                          ),
+                                                        ),
+                                                ),
+                                                Expanded(
+                                                  child: Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            8.0),
+                                                    child: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Text(
+                                                          performer.name,
+                                                          style:
+                                                              GoogleFonts.lato(
+                                                            fontSize: 16.0,
+                                                            fontWeight:
+                                                                FontWeight.w600,
+                                                          ),
+                                                        ),
+                                                        Text(
+                                                          performer.bio
+                                                              .substring(0, 20),
+                                                          style: GoogleFonts.lato(
+                                                              fontSize: 14.0,
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w600,
+                                                              fontStyle:
+                                                                  FontStyle
+                                                                      .italic),
+                                                        ),
+                                                        const SizedBox(
+                                                            height: SizeService
+                                                                .separatorHeight),
+                                                        Text(
+                                                          performer.country,
+                                                          style:
+                                                              GoogleFonts.lato(
+                                                                  fontSize:
+                                                                      14.0,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .w600),
+                                                        ),
+                                                        const SizedBox(
+                                                            height: SizeService
+                                                                .separatorHeight),
+                                                        Text(
+                                                          performer.email,
+                                                          style:
+                                                              GoogleFonts.lato(
+                                                                  fontSize:
+                                                                      14.0,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .w600),
+                                                        ),
+                                                        const SizedBox(
+                                                            height: SizeService
+                                                                .separatorHeight),
+                                                        Text(
+                                                          "Genres",
+                                                          style:
+                                                              GoogleFonts.lato(
+                                                                  fontSize:
+                                                                      14.0,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .w600),
+                                                        ),
+                                                        const SizedBox(
+                                                            height: SizeService
+                                                                .separatorHeight),
+                                                        Wrap(
+                                                          spacing: 6.0,
+                                                          runSpacing: 6.0,
+                                                          children:
+                                                              performer.genre
+                                                                  .map(
+                                                                    (e) => Container(
+                                                                        padding: const EdgeInsets.all(6.0),
+                                                                        decoration: BoxDecoration(
+                                                                          color: ThemeService.isDark(context)
+                                                                              ? DarkTheme.chipBackgroundColor
+                                                                              : LightTheme.chipBackgroundColor,
+                                                                          borderRadius:
+                                                                              BorderRadius.circular(10.0),
+                                                                          boxShadow: [
+                                                                            ThemeService.boxShadow(context),
+                                                                          ],
+                                                                        ),
+                                                                        child: Text(e)),
+                                                                  )
+                                                                  .toList(),
+                                                        )
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    }))
+                                .toList(),
+                          ),
+                        ),
+                        const Divider(),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: feed.content
@@ -73,15 +320,13 @@ class FeedDetailUI extends StatelessWidget {
                                   ))
                               .toList(),
                         ),
-                        const SizedBox(
-                          height: SizeService.separatorHeight * 2,
-                        ),
+                        const Divider(),
                         Text(
                           "Images",
-                          textAlign: TextAlign.center,
+                          textAlign: TextAlign.start,
                           style: GoogleFonts.lato(
-                            fontSize: 20.0,
-                            color: ThemeService.primaryColor,
+                            fontSize: 14.0,
+                            color: ThemeService.secondaryColor,
                             fontWeight: FontWeight.w600,
                           ),
                         ),
